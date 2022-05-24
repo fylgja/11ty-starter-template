c8f4ca479137b8f4d5d3e14ffcdb18f9af981a70
grimlink <sean.grimlink@gmail.com>
2022-05-24 15:23:38 +0200

    DE: trailing slash

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,7 @@
 const isProd = process.env.ELEVENTY_ENV === "prod";
 
 module.exports = {
-    url: isProd ? "https://11ty-fylgja.netlify.app/" : "http://localhost:8080",
+    url: isProd ? "https://11ty-fylgja.netlify.app" : "http://localhost:8080",
     author: "Your Name",
     siteName: "New 11ty Fylgja Blog",
     siteDescription: "Your cool new blog, built with Eleventy and Fylgja CSS.",
